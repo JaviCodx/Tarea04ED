11bd0d22f845f2d9f86f8cf2573de915198c08b5
javie <javie@LAPTOP-B6AB8EK4>
2021-02-06 20:07:30 +0100

    Añadido parametro cantidad en operativa_cuenta

--- deposito/cuentas/CCuenta.java
+++ deposito/cuentas/CCuenta.java
@@ -7,7 +7,39 @@ public class CCuenta {
     private double saldo;
     private double tipoInterAnos;
 
-    public CCuenta()
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCuenta() {
+		return cuenta;
+	}
+
+	public void setCuenta(String cuenta) {
+		this.cuenta = cuenta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public double getTipoInterAnos() {
+		return tipoInterAnos;
+	}
+
+	public void setTipoInterAnos(double tipoInterAnos) {
+		this.tipoInterAnos = tipoInterAnos;
+	}
+
+	public CCuenta()
     {
     }
 
